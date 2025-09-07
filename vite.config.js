2c55dc277bdad4c6436bc2fd9ade41f73cdb564d
Akshat Shukla <aksdec14@gmail.com>
2025-09-07 23:28:09 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(),tailwindcss()],
-  optimizeDeps: {
-    include: ['@teachablemachine/image'],
+   build: {
+    rollupOptions: {
+      external: ["@teachablemachine/image"]
+    }
   }
 })
